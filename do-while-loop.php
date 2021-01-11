f55225f56e5938310781c4570078233ce8a2133e
codewin <codewin19@gmail.com>
2021-01-11 14:55:24 +0530

    Exit Control Loop - do while loop

---
+++ do-while-loop.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0-11/css/all.min.css">
+<link rel="stylesheet" href="style.css">
+<title>While Loop</title>
+</head>
+<body>
+<div class="container">
+    <?php
+    //  init
+    $counter = 1;
+
+   do{
+        // code to repeat
+        echo "<p>".$counter."</p>";
+
+
+        // Incremet
+        $counter ++;
+    } while($counter<=10);
+
+
+?>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
